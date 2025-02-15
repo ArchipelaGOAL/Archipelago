29e7c476024d911580b74232075f65ca846fb8e0
massimilianodelliubaldini <8584296+massimilianodelliubaldini@users.noreply.github.com>
2025-02-15 15:53:50 -0500

    Light cleanup on world.

--- worlds/jakanddaxter/__init__.py
+++ worlds/jakanddaxter/__init__.py
@@ -69,8 +69,8 @@ class JakAndDaxterSettings(settings.Group):
 
     root_directory: RootDirectory = RootDirectory(
         "%programfiles%/OpenGOAL-Launcher/features/jak1/mods/JakMods/archipelagoal")
-    auto_detect_root_directory: typing.Union[AutoDetectRootDirectory, bool] = True
-    enforce_friendly_options: typing.Union[EnforceFriendlyOptions, bool] = True
+    auto_detect_root_directory: AutoDetectRootDirectory | bool = True
+    enforce_friendly_options: EnforceFriendlyOptions | bool = True
 
 
 class JakAndDaxterWebWorld(WebWorld):
@@ -123,7 +123,7 @@ class JakAndDaxterWorld(World):
     """
     # ID, name, version
     game = jak1_name
-    required_client_version = (0, 6, 0)
+    required_client_version = (0, 5, 0)
 
     # Options
     settings: ClassVar[JakAndDaxterSettings]
